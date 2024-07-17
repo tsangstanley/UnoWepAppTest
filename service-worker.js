c66f13be0b0f34c49c02e8cc34fb9dc4f6b651f1
tsangstanley <tsangstanley@gmail.com>
2024-07-17 09:48:44 -0400

    Publish web site

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+import { config } from "/package_d1fd37fdbb164095b2a0abc9f996207e9bdc5628/uno-config.js";
+
+if (config.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True") {
+    console.debug("[ServiceWorker] Initializing");
+
+    self.addEventListener('install', function (e) {
+        console.debug('[ServiceWorker] Installing offline worker');
+        e.waitUntil(
+            caches.open('package_d1fd37fdbb164095b2a0abc9f996207e9bdc5628').then(async function (cache) {
+                console.debug('[ServiceWorker] Caching app binaries and content');
+
+                // Add files one by one to avoid failed downloads to prevent the
+                // worker to fail installing.
+                for (var i = 0; i < config.offline_files.length; i++) {
+                    try {
+                        await cache.add(config.offline_files[i]);
+                    }
+                    catch (e) {
+                        console.debug(`[ServiceWorker] Failed to fetch ${config.offline_files[i]}`);
+                    }
+                }
+            })
+        );
+    });
+
+    self.addEventListener('activate', event => {
+        event.waitUntil(self.clients.claim());
+    });
+
+    self.addEventListener('fetch', event => {
+        event.respondWith(async function () {
+            try {
+                // Network first mode to get fresh content every time, then fallback to
+                // cache content if needed.
+                return await fetch(event.request);
+            } catch (err) {
+                return caches.match(event.request).then(response => {
+                    return response || fetch(event.request);
+                });
+            }
+        }());
+    });
+}
+else {
+    // In development, always fetch from the network and do not enable offline support.
+    // This is because caching would make development more difficult (changes would not
+    // be reflected on the first load after each change).
+    // It also breaks the hot reload feature because VS's browserlink is not always able to
+    // inject its own framework in the served scripts and pages.
+    self.addEventListener('fetch', () => { });
+}
+
+
+// managed
